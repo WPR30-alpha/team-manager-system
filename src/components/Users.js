2b20525264b36dbab45a1331c5c621d4ad39b0a4
Andrew Garvin <dandrewgarvin@gmail.com>
2017-12-16 16:23:17 -0700

    added styling for users table

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,38 +5,35 @@ export default function Users (){
     document.title = "Team Manager | Users"
     return (
         <div className="Users">
-            {/* <div className="filters">
-                <h2>Full Name</h2>
-                <h2>Position</h2>
-                <h2>Status</h2>
-                <h2>Supervisor</h2>
-                <h2>Teams</h2>
-            </div> */}
-            {/* <div className="filter_table"> */}
-                <table className="users_table">
-                    <tr className="filters">
-                        <th>Full Name</th>
-                        <th>Position</th>
-                        <th>Status</th>
-                        <th>Supervisor</th>
-                        <th>Teams</th>
-                    </tr>
-                    <tr className="user_row">
-                        <td>Andrew</td>
-                        <td>Manager, Development</td>
-                        <td>Active</td>
-                        <td>N/A</td>
-                        <td>Admin, Managerial</td>
-                    </tr>
-                    <tr className="user_row" style={{background: '#ddd'}}>
-                        <td>Brennon</td>
-                        <td>Developer, Front End</td>
-                        <td>Inactive</td>
-                        <td>Andrew</td>
-                        <td>None</td>
-                    </tr>
-                </table>
-            {/* </div> */}
+
+            <div className="users_table">
+            <div className="filters table_headers">
+                <h2 className="grid_column">Full Name</h2>
+                <h2 className="grid_column">Position</h2>
+                <h2 className="grid_column">Status</h2>
+                <h2 className="grid_column">Supervisor</h2>
+                <h2 className="grid_column">Teams</h2>
+            </div>
+
+            <div className="table_body">
+                <div className="users_table_row">
+                    <h2 className="users_table_cell">Andrew Garvin</h2>
+                    <h2 className="users_table_cell">Manager, Development</h2>
+                    <h2 className="users_table_cell">Active</h2>
+                    <h2 className="users_table_cell">N/A</h2>
+                    <h2 className="users_table_cell">Development, Manager</h2>
+                </div>
+
+                <div className="users_table_row">
+                    <h2 className="users_table_cell">Brennon Schow</h2>
+                    <h2 className="users_table_cell">Developer, Front End</h2>
+                    <h2 className="users_table_cell">Active</h2>
+                    <h2 className="users_table_cell">Andrew</h2>
+                    <h2 className="users_table_cell">Development</h2>
+                </div>
+            </div>
+
+            </div>
         </div>
     )
 }
